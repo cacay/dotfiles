f2645aa62c3fc1be68e515a2f11327dff7cf5288
Cosku Acay <coskuacay@gmail.com>
2017-05-18 11:34:47 -0700

    Fix vim bundles

--- zsh/0_path.zsh
+++ zsh/0_path.zsh
@@ -1,7 +1,18 @@
 # path, the 0 in the filename causes this to load first
+
+# GO
+export GOPATH="$HOME/gocode"
+
 path=(
   $path
   $HOME/.yadr/bin
   $HOME/.yadr/bin/yadr
+  $HOME/.local/bin
+  $GOPATH/bin
 )
+
+# Nix
+NIX=$HOME/.nix-profile/etc/profile.d/nix.sh
+test -f NIX && . NIX
+
 
